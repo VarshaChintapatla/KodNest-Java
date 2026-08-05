16a6ba8b92de9f775e6bdc9c16dddb8b04d1505c
varsha <chinthapatlavarsha@gmail.com>
2026-08-05 14:48:37 +0530

    added

--- track/Java/M02/t01/t02/P04.java
+++ track/Java/M02/t01/t02/P04.java
@@ -2,10 +2,8 @@ package track.Java.M02.t01.t02;
 
 public class P04 {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 5) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
-            i++;
         }
     }
 }
