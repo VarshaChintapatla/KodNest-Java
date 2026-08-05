033d59064fa2744e84fd56364caca1bc681bc506
varsha <chinthapatlavarsha@gmail.com>
2026-08-05 12:21:29 +0530

    java/M02/T02/P04

---
+++ track/Java/M02/t01/t02/P04.java
@@ -0,0 +1,9 @@
+package track.Java.M02.t01.t02;
+
+public class P04 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
