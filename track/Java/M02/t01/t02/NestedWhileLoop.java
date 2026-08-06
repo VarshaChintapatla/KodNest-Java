c0f5f159bcd5b7a8f74902bea80a2dddc7a60f56
varsha <chinthapatlavarsha@gmail.com>
2026-08-06 11:35:11 +0530

    nestedloops

---
+++ track/Java/M02/t01/t02/NestedWhileLoop.java
@@ -0,0 +1,18 @@
+package track.Java.M02.t01.t02;
+
+public class NestedWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+
+            }
+            i++;
+            System.out.println();
+
+        }
+    }
+}
