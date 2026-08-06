d18777984789175f59284fcf637b9d17c66854d7
varsha <chinthapatlavarsha@gmail.com>
2026-08-06 12:28:38 +0530

    break and continue

---
+++ track/Java/M02/t01/t02/Break.java
@@ -0,0 +1,13 @@
+package track.Java.M02.t01.t02;
+
+public class Break {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ track/Java/M02/t01/t02/Continue.java
@@ -0,0 +1,13 @@
+package track.Java.M02.t01.t02;
+
+public class Continue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+}
