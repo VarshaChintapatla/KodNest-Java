83eb66153ce85985049fbc8cb4a535e5a6977912
varsha <chinthapatlavarsha@gmail.com>
2026-08-14 16:20:41 +0530

    Javapractice mobile,bus

---
+++ track/JavaPractice/Conductor.java
@@ -0,0 +1,41 @@
+package track.JavaPractice.M01.T02;
+
+class Bus {
+
+}
+
+class Money {
+    int money = 50;
+
+}
+
+class Ticket {
+
+}
+
+class Conductor {
+    void collect(Money m) {
+        System.out.println("Money is collected from the passenger");
+    }
+
+    Ticket issueTicket() {
+        Ticket t = new Ticket();
+        return t;
+    }
+}
+
+class Passenger {
+
+    public static void main(String[] args) {
+        Conductor c = new Conductor();
+        Money m = new Money();
+        c.collect(m);
+        Ticket t = c.issueTicket();
+        if (t != null) {
+            System.out.println("Ticket is issued to the passenger");
+        } else {
+            System.out.println("Ticket is not issued to the passenger");
+        }
+    }
+
+}
---
+++ track/JavaPractice/Mobilelock.java
@@ -0,0 +1,56 @@
+package track.JavaPractice.M01.T02;
+
+class Finger {
+
+}
+
+class Face {
+
+}
+
+class Pattern {
+
+}
+
+class Mobile {
+    void unlock(int n) {
+        lockopen();
+    }
+
+    void unlock(String n) {
+        lockopen();
+    }
+
+    void unlock(Finger finger) {
+        lockopen();
+    }
+
+    void unlock(Face face) {
+        lockopen();
+    }
+
+    void unlock(Pattern pattern) {
+        lockopen();
+    }
+
+    void lockopen() {
+        System.out.println("Mobile is unlocked ");
+    }
+}
+
+public class Mobilelock {
+
+    public static void main(String[] args) {
+        Mobile m = new Mobile();
+        Finger finger = new Finger();
+        Face face = new Face();
+        Pattern pattern = new Pattern();
+        m.unlock(123);
+        m.unlock("123");
+        m.unlock(finger);
+        m.unlock(face);
+        m.unlock(pattern);
+
+    }
+
+}
